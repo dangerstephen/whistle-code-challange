f64b008a192a12a061b7ecd969cac94cf7759e49
dangerstephen <dangersteve1@gmail.com>
2017-03-27 16:39:41 -0700

    fixing one of the tests, had meal and walk mixed together

--- spec/models/walk_spec.rb
+++ spec/models/walk_spec.rb
@@ -12,12 +12,12 @@ RSpec.describe Walk, type: :model do
     expect(Walk.count).to eq 1
   end
   it "Walk Doesnt Save..Missing Dog Id" do
-    Meal.create(
-      start_time: "2017-01-01 08:00:00 -0800",
-      end_time: "2017-01-01 08:15:00 -0800",
-      date: "2017-01-01",
-      food: "Royal Canin Medium Puppy Dry"
+    Walk.create(
+    start_time: "2017-01-01 10:00:00 -0800",
+    end_time: "2017-01-01 11:45:00 -0800",
+    date: "2017-01-01",
+    location: "Fort Funston",
     )
-    expect(Meal.count).to eq 0
+    expect(Walk.count).to eq 0
   end
 end
