0391e979af496cf2a9ec1832af8cc89126cc8e5b
dangerstephen <dangersteve1@gmail.com>
2017-03-28 16:49:29 -0700

    fixed a last minute variable change that broke a few test. just fixed it and this is my final commit

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -4,12 +4,14 @@ class Walk < ActiveRecord::Base
     belongs_to :dog
 
     def check_over_lap
-        check_meals = Meal.where(<<-SQL, start_time: start_time, end_time: end_time)
-
-        check_walks = Walk.where(<<-SQL, start_time: start_time, end_time: end_time)
+        meals = Meal.where(<<-SQL, start_time: start_time, end_time: end_time)
         NOT( (start_time > :end_time) || (end_time < :start_time) )
               SQL
-   if check_walks.size > 0 || check_meals.size > 0
+              
+        walks = Walk.where(<<-SQL, start_time: start_time, end_time: end_time)
+        NOT( (start_time > :end_time) || (end_time < :start_time) )
+              SQL
+   if walks.size > 0 || meals.size > 0
         errors.add(:start_time, "Sorry! Another Event is during this same time")
         errors.add(:end_time, "Sorry! Another Event is during this same time")
     end
